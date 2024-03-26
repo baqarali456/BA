bfc999b6132a160443442979b4dc694ff874f6f4
Baqar Ali <sayedalintl@gmail.com>
2024-03-26 15:30:51 +0530

    firstbackend

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('Baqar Login')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('<h1>Baqar In Twitter</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>Youtube Open</h2>')
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+});
